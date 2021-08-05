703f3da96804706ff29d276f67c9f4fb3df8f79c
Hikmet <hikmetiskifoglu@gmail.com>
2021-08-05 15:44:29 +0300

    -Rotater and horizontal obstacle

---
+++ Assets/Scripts/HorizontalObstacleMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalObstacleMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField]float speed,forceSpeed;
+    public bool timer;
+    public float time, interval;
+    void Start()
+    {
+        forceSpeed = speed;
+    }
+
+    
+    void Update()
+    {
+        rb.velocity = Vector3.right * forceSpeed;
+        if (timer)
+        {
+            time += Time.deltaTime;
+            if(time>interval)
+            if(forceSpeed>0)
+            { 
+                forceSpeed = -speed;
+                timer = false;
+                time = 0;
+            }
+            else if(forceSpeed<0)
+            { 
+                forceSpeed = speed;
+                timer = false;
+                time = 0;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "kenar")
+        {
+            timer = true;
+        }
+    }
+}
