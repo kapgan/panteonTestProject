844f7b38847ee7a63d9f5b3966bd6c3aa0bd31e4
Hikmet <hikmetiskifoglu@gmail.com>
2021-08-05 14:14:50 +0300

    -player control and basic level design

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameStatus : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _forwardspeed = 5f;
+
+     float _playerXValue = 0;
+
+    private void Update()
+    {
+            transform.Translate((Input.GetAxis("Horizontal")-_playerXValue) * Time.deltaTime, 0, _forwardspeed * Time.deltaTime);
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "RightRotateObstacle")
+        {
+            Debug.Log("Rigt Enter");
+            _playerXValue = collision.transform.rotation.z;
+        }
+        else if (collision.gameObject.tag == "LeftRotateObstacle")
+        {
+            Debug.Log("Left Enter");
+            _playerXValue = collision.transform.rotation.z;
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            _playerXValue = 0f;
+        }
+    }
+    private void OnCollisionExit(Collision collision) 
+    {
+        if (collision.gameObject.tag == "RightRotateObstacle")
+        {
+            Debug.Log("Rigt Exit");
+           
+        }
+        else if (collision.gameObject.tag == "LeftRotateObstacle")
+        {
+            Debug.Log("Left Exit");
+           
+        }
+    }
+}
